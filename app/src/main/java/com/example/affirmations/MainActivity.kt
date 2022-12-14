53fe1cb226315bffecbd34228014f4cc4d5ea314
KenChen <ken22885@gmail.com>
2022-12-14 10:45:27 +0800

    Create Affirmation list
    1. Use Card composable
    2. Lazy Column composable

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -14,11 +14,28 @@
  * limitations under the License.
  */
 package com.example.affirmations
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,5 +50,49 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AffirmationApp() {
     AffirmationsTheme {
+        AffirmationList(affirmationList = Datasource().loadAffirmations())
     }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(affirmation = affirmation)
+        }
+    }
+}
+
+@Composable
+fun AffirmationCard(
+    modifier: Modifier = Modifier,
+    affirmation: Affirmation,
+) {
+    Card(
+        modifier = modifier.padding(8.dp),
+        elevation = 4.dp,
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop,
+            )
+            Text(
+                text = stringResource(id = affirmation.stringResourceId),
+                modifier = modifier.padding(16.dp),
+                style = MaterialTheme.typography.h6,
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AffirmationCardPreview() {
+    AffirmationCard(affirmation = Affirmation(R.string.affirmation1, R.drawable.image1))
 }
